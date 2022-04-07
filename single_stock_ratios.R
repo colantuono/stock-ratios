0ad8d50dfc6391229f90c9fce7ac9ad4f7ea0915
João Pedro Colantuono <76113840+colantuono@users.noreply.github.com>
2022-04-07 09:26:16 -0300

    Update single_stock_ratios.R
    code cleaning

--- single_stock_ratios.R
+++ single_stock_ratios.R
@@ -13,7 +13,7 @@ if(!require(rbcb)){devtools::install_github("wilsonfreitas/rbcb");library(rbcb)}
 #### MAGIC ####
 ## create the tickers
 tickers = c('^BVSP', 'CSNA3.SA')
-startDate <- "2016-01-01" # periodo de analise
+startDate <- "2016-01-01" ## analysis period
 endDate <- Sys.Date()
 
 ## downloading historical series 
@@ -24,11 +24,11 @@ stock_ret <- as.data.frame(monthlyReturn(CSNA3.SA))
 benchmark_ret <- as.data.frame(monthlyReturn(BVSP))
 
 ## calculating ratios
-juros <- as.data.frame(rbcb::get_series(c(11), last = 1, as = "xts"))[1,1]
+juros <- as.data.frame(rbcb::get_series(c(11), last = 1, as = "xts"))[1,1] ## interest rate
 
 sharpe <- function(mean, interest, stdev){
 (mean - interest) / stdev
-}
+} ## sharpe function
 
 benchmark_mean <- mean(benchmark_ret$monthly.returns)
 benchmark_variance <- var(benchmark_ret$monthly.returns)
@@ -39,7 +39,6 @@ stock_mean <- mean(stock_ret$monthly.returns)
 stock_variance <- var(stock_ret$monthly.returns)
 stock_stdev <- sd(stock_ret$monthly.returns)
 stock_sharpe <- sharpe(stock_mean, juros, stock_stdev)
-
 
 benchmark_vec <- benchmark_ret[1:nrow(stock_ret),]
 covar <- cov(benchmark_vec, stock_ret$monthly.returns)
@@ -57,13 +56,3 @@ benchmark_sharpe # average return in excess of risk-free rate
 stock_sharpe # average return in excess of risk-free rate
 Beta # volatility of a stock compared to the benchmark index
 Alpha # excess returns above benchmark
-
-
-# benchmark_sharpe
-# VALE_sharpe
-# VALE_Beta
-# VALE_Alpha
-# CSNA_sharpe
-# CSNA_Beta
-# CSNA_Alpha
-
